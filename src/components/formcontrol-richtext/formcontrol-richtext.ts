174f462a61ce1342133affe29d0553ad6e964cf5
James West <judgewest2000@hotmail.co.uk>
2017-05-07 00:51:53 +0100

    ratified the formatting

--- src/components/formcontrol-richtext/formcontrol-richtext.ts
+++ src/components/formcontrol-richtext/formcontrol-richtext.ts
@@ -1,5 +1,6 @@
 import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { IFormControlDefinition } from '../../modelinterfaces/base';
+import { StringHelper } from '../../helpers/string-helper';
 
 /**
  * Generated class for the FormcontrolRichtext component.
@@ -14,6 +15,7 @@ import { IFormControlDefinition } from '../../modelinterfaces/base';
 export class FormcontrolRichtext {
 
   @ViewChild('editor') editor: ElementRef;
+  @ViewChild('decorate') decorate: ElementRef;
 
   @Input() definition: IFormControlDefinition;
 
@@ -21,12 +23,26 @@ export class FormcontrolRichtext {
 
   }
 
-  ngAfterContentInit() {
+  private wireupResize() {
+
+    let element = this.editor.nativeElement as HTMLDivElement;
+
+    let height = (window.innerHeight || document.body.clientHeight) - 250;
+    let textareaHeight = Math.round((height / 100.00) * 45);
+    element.style.height = `${textareaHeight}px`;
+
+  }
+
+  private updateItem() {
     let element = this.editor.nativeElement as HTMLDivElement;
     element.innerHTML = this.definition.formControlItem.value;
 
+    if (StringHelper.isNullOrWhiteSpace(element.innerHTML)) {
+      element.innerHTML = '<div></div>';
+    }
+
     let updateItem = () => {
-      let sanitizedHtml = element.innerHTML.replace(/<(\/?|\!?)(DOCTYPE html|html|head|body|div)>/g, "");
+      let sanitizedHtml = element.innerHTML;//= element.innerHTML.replace(/<(\/?|\!?)(DOCTYPE html|html|head|body|div)>/g, "");
       this.definition.formControlItem.setValue(sanitizedHtml);
     };
 
@@ -35,14 +51,37 @@ export class FormcontrolRichtext {
     element.onpaste = () => updateItem();
     element.oninput = () => updateItem();
 
+
   }
 
-  ngAfterContentChecked() {
-    console.log('AfterContentChecked');
+  private wireupButtons() {
+    let buttons = (this.decorate.nativeElement as HTMLDivElement).getElementsByTagName('button');
+    for (let i = 0; i < buttons.length; i++) {
+      let button = buttons[i];
+
+      let command = button.getAttribute('data-command');
+
+      if (StringHelper.contains(command, '|')) {
+        let parameter = command.split('|')[1];
+        command = command.split('|')[0];
+
+        button.addEventListener('click', () => {
+          document.execCommand(command, false, parameter);
+        });
+      } else {
+        button.addEventListener('click', () => {
+          document.execCommand(command);
+        });
+      }
+    }
+
   }
 
-  ngAfterViewInit() {
-    console.log('AfterContentChecked');
+  ngAfterContentInit() {
+    this.wireupResize();
+    this.updateItem();
+    this.wireupButtons();
   }
+
 
 }
