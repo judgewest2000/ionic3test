1099e1b79a11f4503f46609394fcb2edec9d022f
James West <judgewest2000@hotmail.co.uk>
2017-05-06 22:20:12 +0100

    moved all the standard stuff from release-edit into a base-edit lib

---
+++ src/pages/base-edit/base-edit.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ModalService } from '../../providers/modal-service';
+import { IForm, IFormControlDefinition } from '../../modelinterfaces/base';
+import { FormControl } from '@angular/forms';
+import { BaseEntityService } from '../../providers/baseentity-service';
+
+export abstract class BaseEdit<T extends AIMC.Baltic.Dto.RestrictedVisibilityDtoRootBase> {
+
+    item: IForm<T>;
+    itemLoaded = false;
+
+    constructor(private params: {
+        navCtrl: NavController,
+        navParams: NavParams,
+        modalService: ModalService,
+        baseEntityService: BaseEntityService<T>,
+        entityTitle: string
+    }) {
+    }
+
+    submitAttemptMade = false;
+
+    createFormControlDefinition(title: string, formControlItem: FormControl) {
+        return <IFormControlDefinition>{
+            title: title,
+            formControlItem: formControlItem,
+            submitAttemptMade: this.submitAttemptMade
+        };
+    }
+
+    async getUsingNavId() {
+        this.params.modalService.turnOnLoading();
+        let id = parseInt(this.params.navParams.get('id'));
+        let form = await this.params.baseEntityService.getForm(id);
+        this.item = form;
+        this.itemLoaded = true;
+        this.params.modalService.turnOffLoading();
+    }
+
+    delete() {
+        this.params.modalService.confirm({
+            title: 'Confirm Deletion',
+            body: 'Confirm delete of this release',
+            confirmCallback: () => {
+                this.params.baseEntityService.delete(this.item.viewModel)
+                    .then(() => {
+                        this.params.navCtrl.pop();
+                    });
+            }
+        });
+    }
+
+    async save() {
+
+        this.submitAttemptMade = true;
+
+        if (this.item.form.invalid) {
+            this.params.modalService.alert({
+                title: `Cannot save ${this.params.entityTitle}`,
+                body: 'Please check all fields are valid'
+            });
+        } else {
+            try {
+                this.params.modalService.turnOnLoading();
+                let release = await this.params.baseEntityService.save(this.item);
+                this.item.viewModel = release;
+                this.params.modalService.turnOffLoading();
+
+                this.params.modalService.alert({
+                    title: `${this.params.entityTitle} saved`,
+                    body: `Your ${this.params.entityTitle} has been saved successfully`,
+                    confirmCallback: () => {
+                        this.params.navCtrl.pop();
+                    }
+                });
+
+            } catch (ex) {
+                this.params.modalService.alert({
+                    title: `Issue saving your ${this.params.entityTitle}`,
+                    body: `Error: ${JSON.stringify(ex)}: If this happens again please contact Vuelio Support`
+                });
+            }
+
+        }
+    }
+
+    cancel() {
+        this.params.navCtrl.pop();
+    }
+
+
+}
--- src/pages/release-edit/release-edit.ts
+++ src/pages/release-edit/release-edit.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ReleaseService } from '../../providers/release-service';
 import { ModalService } from '../../providers/modal-service';
-import { IForm, IFormControlDefinition } from '../../modelinterfaces/base';
-import { FormControl } from '@angular/forms';
 
+
+import { BaseEdit } from '../base-edit/base-edit';
 
 @IonicPage({
   segment: 'release-edit/:id'
@@ -13,14 +13,9 @@ import { FormControl } from '@angular/forms';
   selector: 'page-release-edit',
   templateUrl: 'release-edit.html',
 })
-export class ReleaseEdit {
+export class ReleaseEdit extends BaseEdit<AIMC.Baltic.Dto.MediaDatabase.ReleaseDto>{
 
   segment = 'main';
-
-  item: IForm<AIMC.Baltic.Dto.MediaDatabase.ReleaseDto>;
-  submitAttemptMade = false;
-
-  releaseLoaded = false;
 
   constructor(
     public navCtrl: NavController,
@@ -28,94 +23,32 @@ export class ReleaseEdit {
     private releaseService: ReleaseService,
     private modalService: ModalService) {
 
-    modalService.turnOnLoading();
-
-    let id = parseInt(this.navParams.get('id'));
-
-    releaseService.getForm(id).then(value => {
-
-      if (id === 0) {
-        value.form.controls['displayDateTime'].setValue(new Date().toISOString());
-        value.form.controls['scheduledDateTime'].setValue(new Date().toISOString());
-      }
-
-      this.item = value;
-      this.releaseLoaded = true;
-      modalService.turnOffLoading();
-      this.updateHeadline();
+    super({
+      navCtrl: navCtrl,
+      navParams: navParams,
+      modalService: modalService,
+      baseEntityService: releaseService,
+      entityTitle: 'Release'
     });
 
-  }
-
-  createFormControlDefinition(title: string, formControlItem: FormControl) {
-    return <IFormControlDefinition>{
-      title: title,
-      formControlItem: formControlItem,
-      submitAttemptMade: this.submitAttemptMade
-    };
-  }
-
-
-  delete() {
-    this.modalService.confirm({
-      title: 'Confirm Deletion',
-      body: 'Confirm delete of this release',
-      confirmCallback: () => {
-        this.releaseService.delete(this.item.viewModel)
-          .then(() => {
-            this.navCtrl.pop();
-          });
+    super.getUsingNavId().then(() => {
+      if (this.item.viewModel.id === 0) {
+        this.item.form.controls['displayDateTime'].setValue(new Date().toISOString());
+        this.item.form.controls['scheduledDateTime'].setValue(new Date().toISOString());
+        this.updateHeadline();
       }
     });
   }
 
-  cancel() {
-    this.navCtrl.pop();
-  }
-
-
-  async save() {
-
-    this.submitAttemptMade = true;
-
-    if (this.item.form.invalid) {
-      this.modalService.alert({
-        title: 'Cannot save release',
-        body: 'Please check all fields are valid'
-      });
-    } else {
-      try {
-        this.modalService.turnOnLoading();
-        let release = await this.releaseService.save(this.item);
-        this.item.viewModel = release;
-        this.modalService.turnOffLoading();
-
-        this.modalService.alert({
-          title: 'Release saved',
-          body: 'Your release has been saved',
-        });
-
-      } catch (ex) {
-        this.modalService.alert({
-          title: 'Issue saving your release',
-          body: `Could not save release: ${JSON.stringify(ex)}`
-        });
-      }
-
-    }
-  }
 
   displayDelete() {
-
     return this.item.viewModel.id !== 0;
   }
 
   //KEEP THE HEADLINE AND NAME INLINE WITH EACH OTHER SAME AS NORMAL SYSTEM
   _previousName = '';
   updateHeadline() {
-
     this._previousName = this.item.form.controls['name'].value;
-
     this.item.form.controls['name'].valueChanges.subscribe((name: string) => {
       if (this.item.form.controls['headline'].value === this._previousName) {
         this.item.form.controls['headline'].setValue(name);
