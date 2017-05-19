0d130ac9ba2d54c3001d77fbe8683031a9ed1c5d
James West <judgewest2000@hotmail.co.uk>
2017-05-19 14:22:11 +0100

    re-introduced the normal page transition for ios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { ProvidersModule } from '../providers/providers.module';
             platforms: {
                 ios: {
                     //backButtonText: ''
-                    pageTransition: 'md-transition'
+                    //pageTransition: 'md-transition'
                 }
             }
         }),
